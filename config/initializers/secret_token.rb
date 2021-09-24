a9552b81f642c94d2362b3304423ea17dc16cc26
MeetChavda <chavdameet493@gmail.com>
2021-09-24 23:08:43 +0530

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SecureGlass::Application.config.secret_token = '33d872878aa4486f9fe584943f22440ff7f322f57054aacc3a6357ce0aeb3d340bff1c473f7fd5b3a6b8b771d23c3a612d4c9f3c469c6d4c81562e193bc3b518'
